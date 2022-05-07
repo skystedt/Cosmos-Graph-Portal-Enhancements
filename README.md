53c00d9626a84e1f703ba403cffb395d5051033c
Skystedt <hampus@skystedt.se>
2022-05-07 16:20:18 +0200

    Version 2.0.0 - Ability to format the query - Auto resizess the height of the query window - Enable tab (insert two spaces) - Disable spellchecking (no red squiggles) - Hide query completion suggestions

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Cosmos Graph Portal Enhancements
 Chrome extension to enhance the Cosmos graph query editor in the portal.
 
+- Ability to format the query
+- Auto resizess the height of the query window
 - Enable line breaks  
-  (enter or shift + enter will insert at line break, ctrl + enter executes the query)
+  (enter or shift + enter will insert a line break, ctrl + enter executes the query)
+- Enable tab (insert two spaces)
 - Enable right click in the editor text field
+- Disable spellchecking (no red squiggles)
+- Hide query completion suggestions
+
+![Screenshot](Screenshot.png)
 
 [Chrome Web Store](https://chrome.google.com/webstore/detail/kafgakbjaooedlknboeegpjeiddfofpb)
--- src/cosmos.js
+++ src/cosmos.js
@@ -1,61 +1,254 @@
 "use strict";
 
-(function () {
-  const tabPanesSelector = ".tabPanesContainer";
-  const queryEditorSelector = ".queryContainer textarea";
+class QueryEditor {
+  #textAreaAddedCallback;
+  #keydownCallback;
+  #inputCallback;
+  #contextMenuCallback;
 
-  function observeFirstBodyMutation() {
+  #textarea;
+
+  #buttonContainer;
+  #buttons = [];
+  
+  get query() {
+    if (this.#textarea) {
+      return this.#textarea.value;
+    }
+    return null;
+  }
+  set query(value) {
+    if (this.#textarea) {
+      this.#textarea.value = value;
+      this.#textarea.dispatchEvent(new Event("change", { bubbles: true }));
+    }
+  }
+
+  constructor() {
+    this.#observeFirstBodyMutation();
+  }
+  
+  textAreaAdded(callback) {
+    this.#textAreaAddedCallback = callback;
+  }
+  
+  input(callback) {
+    this.#inputCallback = callback;
+  }
+  
+  keydown(callback) {
+    this.#keydownCallback = callback;
+  }
+  
+  contextMenu(callback) {
+    this.#contextMenuCallback = callback;
+  }
+  
+  addButton(button) {
+    this.#buttons.push(button);
+    this.#createButton(button);
+  }
+  
+  resizeToContent() {
+    // https://stackoverflow.com/a/25621277
+    this.#textarea.style.height = "auto";
+    this.#textarea.style.height = (this.#textarea.scrollHeight + 2) + "px";
+  }
+
+  addStyle(style) {
+    document.head.insertAdjacentHTML("beforeend", `<style>${style}</style>`);
+  }
+
+  #queryTextAreaAdded(element) {
+    this.#textarea = element;
+
+    element.addEventListener("keydown", (event) => {
+      if (this.#keydownCallback) {
+        return this.#keydownCallback(event);
+      }
+    });
+    
+    element.addEventListener("input", (event) => {
+      if (this.#inputCallback) {
+        return this.#inputCallback(event);
+      }
+    });
+    
+    document.addEventListener("contextmenu", (event) => {
+      const queryTextAreaSelector = ".queryContainer textarea";
+      if (event.target.matches(queryTextAreaSelector)) {
+        if (this.#contextMenuCallback) {
+          return this.#contextMenuCallback(event);
+        }
+      }
+    }, true);
+    
+    if (this.#textAreaAddedCallback) {
+      return this.#textAreaAddedCallback(element);
+    }
+  };
+
+  #queryButtonAdded(element) {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.style.display = "flex";
+    buttonContainer.style.flexDirection = "column";
+    buttonContainer.style.justifyContent = "space-between";
+    element.replaceWith(buttonContainer);
+    buttonContainer.appendChild(element);
+
+    this.#buttonContainer = buttonContainer;
+
+    this.#buttons.forEach((button) => {
+      this.#createButton(button);
+    })
+  };
+
+  #createButton(buttonProperties) {
+    if (this.#buttonContainer) {
+      const button = document.createElement("button");
+      button.classList.add("filterbtnstyle", "queryButton");
+      button.textContent = buttonProperties.text;
+      button.style = buttonProperties.style;
+      if (buttonProperties.click) {
+        button.onclick = buttonProperties.click;
+      }
+      if (buttonProperties.first == true) {
+        this.#buttonContainer.prepend(button);
+      } else {
+        this.#buttonContainer.append(button);
+      }
+    }
+  }
+  
+  #observeFirstBodyMutation() {
     const bodyObserver = new MutationObserver((mutationsList, observer) => {
       for (const mutation of mutationsList) {
         for (const node of mutation.addedNodes) {
           observer.disconnect();
-          elementAddedToBody(node);
+          this.#elementAddedToBody(node);
         }
       }
     });
     bodyObserver.observe(document.body, { subtree: false, childList: true });
   }
 
-  function elementAddedToBody(element) {
+  #elementAddedToBody(element) {
+    const tabPanesSelector = ".tabPanesContainer";
+
     const tabsObserver = new MutationObserver((mutationsList, observer) => {
       for (const mutation of mutationsList) {
         for (const node of mutation.addedNodes) {
-          tabElementAdded(node);
+          this.#tabElementAdded(node);
         }
       }
     });
+
     const tabPanesContainer = element.querySelector(tabPanesSelector);
     if (!tabPanesContainer) {
       console.warn("Tab panes element not found");
       return;
     }
+
     tabsObserver.observe(tabPanesContainer, { subtree: false, childList: true });
   }
 
-  function tabElementAdded(tabElement) {
-    const queryEditor = tabElement.querySelector(queryEditorSelector);
+  #tabElementAdded(tabElement) {
+    const queryContainerSelector = ".graphExplorerContainer .queryContainer";
+  
+    const queryContainer = tabElement.querySelector(queryContainerSelector);
+    if (!queryContainer) {
+      console.warn("Query container element not found");
+      return;
+    }
+
+    const queryEditor = queryContainer.querySelector("textarea");
     if (!queryEditor) {
       console.warn("Query editor element not found");
       return;
     }
-    queryEditor.addEventListener("keydown", queryEditorKeydown);
+
+    const queryButton = queryContainer.querySelector("button.queryButton");
+    if (!queryButton) {
+      console.warn("Query button element not found");
+      return;
+    }
+
+    this.#queryTextAreaAdded(queryEditor);
+    this.#queryButtonAdded(queryButton);
+  }
+}
+
+class GremlinFormatter {
+  #gremlint;
+
+  async load() {
+    this.#gremlint = await import(chrome.runtime.getURL("gremlint@v3.6.0.js"));
   }
 
-  function queryEditorKeydown(event) {
+  format(query) {
+    const queryLength = query.replaceAll(/\n */g, "").length;
+    return this.#gremlint.formatQuery(query, {
+      indentation: 0,
+      maxLineLength: Math.max(Math.min(160, queryLength - 1), 0),
+      shouldPlaceDotsAfterLineBreaks: true
+    });
+  }
+}
+
+(async () => {
+  const queryEditor = new QueryEditor();
+  
+  queryEditor.textAreaAdded((element) => {
+    
+    // disable spellcheck
+    element.spellcheck = false;
+    
+    // remove rezise (since we auto size after content) and set min height
+    element.style.resize = "none";
+    element.style.minHeight = "84px";
+    element.parentNode.style.minHeight = "inherit"; // default has a min height smaller than the textarea
+  });
+  
+  queryEditor.keydown((event) => {
+    // enable new lines
     if (event.key === "Enter" && !event.ctrlKey) {
       event.stopPropagation();
       return true;
     }
-  }
+    
+    // change tab to spaces (to prevent tabbing out of the textarea)
+    if (event.key === "Tab") {
+      event.preventDefault();
+      document.execCommand("insertText", false, "  "); // execCommand is deprecated, but there is no good alternative for inserting text and also preserving undo/redo history
+    }
+  });
+  
+  queryEditor.input((event) => {
+    // rezise the editor to the query
+    queryEditor.resizeToContent();
+  });
 
-  function enableQueryEditorContextMenu() {
-    document.addEventListener("contextmenu", (e) => {
-      if (e.target.matches(queryEditorSelector)) {
-        e.stopPropagation();
-      }
-    }, true);
-  }
+  queryEditor.contextMenu((event) => {
+    // enable query editor context menu
+    event.stopPropagation();
+  });
 
-  observeFirstBodyMutation();
-  enableQueryEditorContextMenu();
+  const gremlinFormatter = new GremlinFormatter();
+  await gremlinFormatter.load();
+
+  // add format query button
+  queryEditor.addButton({
+    text: "Format Query",
+    style: "background: #66aaaa",
+    first: true,
+    click: () => {
+      queryEditor.query = gremlinFormatter.format(queryEditor.query);
+      // rezise the editor to the query
+      queryEditor.resizeToContent();
+    }
+  });
+  queryEditor.addStyle(".filterbtnstyle.queryButton:hover { border: solid 1px transparent; }");
+  
+  // hide suggestion list
+  queryEditor.addStyle(".graphExplorerContainer .queryContainer .ms-List { display: none; }");
 })();
